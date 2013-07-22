396dcfbebc7bfa739956ec3f6e7dc75a9df436b7
stef <stefan.marsiske@gmail.com>
2013-07-22 22:39:35 +0200

    [enh] speed up dedup and avoid spurious duplicate marks

--- lobbyfacts/data/transform/dedup.py
+++ lobbyfacts/data/transform/dedup.py
@@ -6,13 +6,17 @@ log = logging.getLogger(__name__)
 
 def dedup_fields(engine, field):
     table = sl.get_table(engine, 'representative')
+    seen=set([])
     for n, rep in enumerate(sl.all(engine, table)):
         if n % 100 == 0:
             print n, 'done'
+        if not rep[field] or rep[field] in seen: continue
+        seen.update(rep[field])
         others = list(sl.find(engine, table, **{field: rep[field]}))
         if len(others) > 1:
             log.info("Duplicates for: %s", rep['name'])
             for i, re in enumerate(others):
+                if re == rep: continue
                 text = "(Duplicate %s)" % (i+1)
                 sl.upsert(engine, table,
                     {'name_suffix': text,
