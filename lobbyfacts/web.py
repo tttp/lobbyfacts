9c437623bff9b4eb90f3e1eb8bdec05d44bce75d
stef <stefan.marsiske@gmail.com>
2013-07-27 23:11:25 +0200

    [enh] added new reports to api

--- lobbyfacts/web.py
+++ lobbyfacts/web.py
@@ -68,7 +68,9 @@ def companies_by_exp(report):
     if report in ['companies_by_exp', 'tradeassoc_by_exp',
         'consultancies_by_turnover', 'lawfirms_by_turnover',
         'companies_by_fte', 'ngos_by_fte', 'tradeassocs_by_fte',
-        'lawfirms_by_fte', 'fte_by_category', 'rep_by_country']:
+        'lawfirms_by_fte', 'fte_by_category', 'rep_by_country',
+        'unregistered', 'reps_by_accredited', 'accreditted_by_cat',
+        'reps_by_members', reps]:
         return render_template('reports/%s.tmpl' % report)
     raise NotFound()
 
